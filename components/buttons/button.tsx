11d51c53424bf568fcd21d8070af3bbff3e32868
kev <kbreto2911@gmail.com>
2022-04-07 18:28:59 -0400

    update: filterScreen

--- components/buttons/button.tsx
+++ components/buttons/button.tsx
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function Button( {children, className,...props} : React.ButtonHTMLAttributes<HTMLButtonElement>){
   return(
-    <button className={["bg-secondary h-12 p-3 rounded-lg flex justify-center items-center",
-     className].join(' ')} {...props} >
+    <button className={["h-12 p-3 rounded-lg flex justify-center items-center",
+     className, props.disabled ? 'bg-gray-200' : 'bg-secondary '].join(' ')} {...props} >
     {children}
     </button>
   )
--- pages/itemList.tsx
+++ pages/itemList.tsx
@@ -6,7 +6,7 @@ import ProductCard, { ProductCardProps } from "components/productCard"
 import Head from "next/head"
 import Image from "next/image"
 import { MouseEventHandler, useEffect, useState } from "react"
-
+// https://www.figma.com/file/SkNIiseakx6UzQI8PkZMNs/New-Aliolam?node-id=514%3A7592
 
 export default function ItemList(){
   const items = Array<ProductCardProps>(12).fill({
@@ -16,12 +16,12 @@ export default function ItemList(){
     price: "49.00"
   })
 
-  const [sortScreen, setSortScreen] = useState(true);
-  const toggleSortScreen : MouseEventHandler<HTMLDivElement> = (ev) => {
-    if(ev.currentTarget === ev.target){
-      setSortScreen(prev => !prev);
-    }
-  }
+  const [sortScreen, setSortScreen] = useState(false);
+  const toggleSortScreen = () => setSortScreen(prev => !prev);
+   
+
+  const [filterScreen, setFilterScreen] = useState(false);
+  const toggleFilterScreen = () => setFilterScreen(prev => !prev)
   return(
     <div  className="relative flex min-h-screen flex-col overflow-hidden w-full">
       <Head>
@@ -30,10 +30,19 @@ export default function ItemList(){
       </Head>
       <Navegation />
       <Header />
-      <Filter sortHandler={toggleSortScreen} />
-      <FilterTags tags={['Ubicacion 1', '$50.00-$100.00']} />
-      <List items={items} />
+      <FilterButtons sortHandler={toggleSortScreen} filterHandler={toggleFilterScreen} />
+      <div className="w-full flex lg:px-20">
+        <div className="w-77 hidden lg:flex lg:flex-col mr-4">
+          <Filter />
+        </div>
+        <div className="flex-1">
+        <DesktopSort />
+        <FilterTags tags={['Ubicacion 1', '$50.00-$100.00']} />
+        <List items={items} />
+        </div>
+      </div>
       {sortScreen && <SortScreen closeHandler={toggleSortScreen} />}
+      {filterScreen && <FilterScreen backHandler={toggleFilterScreen} />}
       <Footer />
     </div>
   )
@@ -49,16 +58,17 @@ const Header = () => {
   )
 }
 
-const Filter = (props : {
-  sortHandler?: MouseEventHandler<HTMLDivElement>
+const FilterButtons = (props : {
+  sortHandler?: MouseEventHandler<HTMLDivElement>,
+  filterHandler? : MouseEventHandler<HTMLDivElement>
 }) => {
   return(
-    <div className="w-full flex border-b border-gray-400 mb-6">
+    <div className="w-full flex border-b border-gray-400 mb-6 lg:hidden">
       <div onClick={props.sortHandler} className="flex-1 border-r border-gray-400 flex flex-col items-center py-1 cursor-pointer">
         <Image className="mb-1" src={require('public/icons/switch-vertical-black.png')} width={24} height={24} />
         <p className="text-title-color text-semibold text-sm" >Ordenar</p>
       </div>
-      <div className="flex-1 flex flex-col items-center py-1 cursor-pointer">
+      <div onClick={props.filterHandler} className="flex-1 flex flex-col items-center py-1 cursor-pointer">
         <Image className="mb-1" src={require('public/icons/adjustments-black.png')} width={24} height={24} />
         <p className="text-title-color text-semibold text-sm" >Filtrar</p>
       </div>
@@ -129,7 +139,13 @@ const ProgressBar = (props : {
 const SortScreen = (props:{
   closeHandler? : MouseEventHandler<HTMLDivElement>
 }) => {
+  const _closeHandler : MouseEventHandler<HTMLDivElement> = (ev) => {
+    if(props.closeHandler && ev.currentTarget === ev.target){
+      props.closeHandler(ev)
+    }
+  }
   const [currentOption, setCurrentOption] = useState(0)
+
   const sortEffects = ['Orden por defecto', 'Menor precio', 'Mayor precio'];
   const sortOptions = sortEffects.map((text, i) => {
     const [active, setActive] = useState(false);
@@ -145,11 +161,104 @@ const SortScreen = (props:{
     )
   })
 
-  
   return(
-    <div onClick={props.closeHandler} className="fixed z-10 h-screen w-screen bg-[#0006] flex items-center pl-4 pr-8" >
+    <div onClick={_closeHandler} className="fixed z-10 h-screen w-screen bg-[#0006] flex items-center pl-4 pr-8" >
       <div className="w-full h-36 border-b border-gray-400 bg-white">
         {sortOptions}
+      </div>
+    </div>
+  )
+}
+
+
+const filtersInformation = [
+  {title : "Categorias", filters: ["Categoria 1", "Categoria 2", "Categoria 3", "Categoria 4", "Categoria 5"]},
+  {title : "Precio", filters: ["$50.00-$100.00", "$100.00-$200.00", "$200.00-$300.00"]},
+  {title : "Ubicacion", filters: ["Ubicacion 1", "Ubicacion 2", "Ubicacion 3", "Ubicacion 4",]},
+]
+
+const Filter = () => {
+  const _filter = filtersInformation.map((filter, i) => {
+    const [active, setActive] = useState(false);
+    const toggleActive = () => setActive(prev => !prev);
+  
+    const optionsJSX = filter.filters.map((option, i) =>{
+      const [activeFilter, setActiveFilter] = useState(false)
+      const toggleActiveFilter = () => setActiveFilter(prev => !prev)
+      return(
+        <div key={i} onClick={toggleActiveFilter} className="cursor-pointer hover:bg-gray-300 relative h-12 px-4 flex items-center ">
+          {activeFilter ?
+          <div  className="h-4 w-4 mr-4 rounded-full bg-white border-4 border-secondary"></div>:
+          <div  className="h-4 w-4 mr-4 rounded-full bg-white border border-black"></div> }
+          <p>{option}</p>
+        </div>
+      )
+    })
+    return(
+      <div key={i} className="w-full">
+        <div onClick={toggleActive} className="cursor-pointer hover:bg-gray-300 relative h-12 px-4 flex items-center border-b border-gray-200">
+          <p className="text-p-color text-semibold">{filter.title}</p>
+          <div className="absolute right-4" >
+            <Image src={require('public/icons/pointer-down-black.png')} width={16} height={16} />
+          </div>
+        </div>
+        {active && 
+        <div className="mt-4">
+          {optionsJSX}
+        </div>}
+      </div>
+    )
+  })
+  return(
+    <>
+      {_filter}
+    </>
+  )
+}
+
+const FilterScreen = (props : {
+  backHandler?: MouseEventHandler<HTMLDivElement>
+}) => {
+  
+  return(
+    <div className="fixed z-10 h-screen w-screen bg-white overflow-y-auto flex flex-col justify-between" >
+      <div className="w-full">
+        <div className="w-full px-7 py-6 bg-primary" >
+          <div className="flex items-center" onClick={props.backHandler} >
+            <Image className="cursor-pointer" src={require('public/icons/arrow-left.png')} width={24} height={24} />
+          </div>
+        </div>
+        <h1 className="mx-4 my-6 text-3xl text-title-color text-bold">Filtrar por:</h1>
+        <div className="w-full ">
+          <Filter />
+        </div>
+      </div>
+      <div className="w-full pb-8 px-4 flex flex-col items-center cursor-pointer" >
+        <p className="underline mb-4">Limpiar filtros</p>
+        <Button className="w-full cursor-pointer" >
+          <p className="text-white text-sm text-semibold" >Mostrar resultados</p>
+        </Button>
+      </div>
+
+    </div>
+  ) 
+}
+
+const DesktopSort = () => {
+
+  return(
+    <div className="w-full lg:flex hidden border-b pb-5 border-gray-200">
+      <div className="text-xs text-p-color">(Mostrando 1-12 productos de 48 productos)</div>
+      <div className="flex">
+        <p className="text-bold text-sm text-title-color">Mostrar:</p>
+        <div className="border border-gray-200 rounded-lg p-2 flex items-center">
+          <p className="text-sm text-p-color">12</p>
+          <Image src={require('public/icons/pointer-down-black.png')} width={16} height={16} />
+        </div>
+        <div className="border border-gray-200 rounded-lg p-2 flex items-center">
+          <p className="text-sm text-p-color">Orden por defecto</p>
+          <Image src={require('public/icons/pointer-down-black.png')} width={16} height={16} />
+        </div>
       </div>
     </div>
   )
